80703d24c2aa895a3fb4624b07585b179571748c
protermarad-hash <proterm.arad@gmail.com>
2026-06-07 14:16:41 +0300

    Show WhatsApp quick contact button globally

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -3,12 +3,14 @@
 import { CartProvider } from '@/lib/cart-context';
 import CartDrawer from './CartDrawer';
 import CookieConsent from './CookieConsent';
+import WhatsAppFloat from './WhatsAppFloat';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CartProvider>
       {children}
       <CartDrawer />
+      <WhatsAppFloat />
       <CookieConsent />
     </CartProvider>
   );
