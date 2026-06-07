ceeeb6ea8a383ff64ed3aefbcf0bde29ce390d0e
protermarad-hash <proterm.arad@gmail.com>
2026-06-07 12:50:34 +0300

    Render cookie consent banner globally

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,12 +2,14 @@
 
 import { CartProvider } from '@/lib/cart-context';
 import CartDrawer from './CartDrawer';
+import CookieConsent from './CookieConsent';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <CartProvider>
       {children}
       <CartDrawer />
+      <CookieConsent />
     </CartProvider>
   );
 }
